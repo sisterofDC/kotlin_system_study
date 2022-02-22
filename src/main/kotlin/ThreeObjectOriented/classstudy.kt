5c0b579a954253976f72894c995e25fd1f670ff0
sisterofDC <90042818+sisterofDC@users.noreply.github.com>
2022-02-22 18:31:18 +0800

    testone

--- src/main/kotlin/ThreeObjectOriented/classstudy.kt
+++ src/main/kotlin/ThreeObjectOriented/classstudy.kt
@@ -3,10 +3,17 @@ package ThreeObjectOriented
 //属性默认值
 //不同的可访问修饰符 kotlin类中的成员默认是全局可见
 
-class classstudy {
-
+class classstudy(
+    val sale:Int,
+    val buyer:Int,
+    val price:Double
+){
+    init {
+        var i:Int=0
+        print("this is buyer ${i}")
+    }
 }
 
 interface fly{
-    
+
 }
